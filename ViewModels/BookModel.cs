a541641482b9941a40ce620aeed6cbdd960d02ae
MustAAGGH <sedathasanov21@gmail.com>
2023-01-06 22:30:07 +0200

    View modeli

---
+++ ViewModels/BookModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ViewModels
+{
+    public class BookModel
+    {
+        public string Title { get; set; }
+        public int PublishingYear { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+    }
+}

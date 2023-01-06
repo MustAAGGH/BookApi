1b5525fe10b1df7c047ae441634a36d91bfbfa7f
MustAAGGH <sedathasanov21@gmail.com>
2023-01-06 20:11:06 +0200

    Modeli

---
+++ Data/Books.cs
@@ -0,0 +1,13 @@
+﻿namespace Data
+{
+    public class Books
+    {
+        public int Id { get; set; }
+        public string Title { get ; set; }
+        public int PublishingYear { get; set; }
+        public int AuthorId { get; set; }
+        public Authors Author { get; set; }
+        public int GenreId { get; set; }
+        public Genres Genre { get; set; }
+    }
+}

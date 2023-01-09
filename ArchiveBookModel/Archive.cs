86e9b02c8b2ceedc2de5cfda3ca8d02b3c78d6b5
MustAAGGH <sedathasanov21@gmail.com>
2023-01-09 16:32:18 +0200

    Inserting data to database

---
+++ ArchiveBookModel/Archive.cs
@@ -0,0 +1,26 @@
+﻿using ViewModels;
+
+namespace ArchiveBookModel
+{
+    public static class Archive
+    {
+        public static List<BookModel> ArchiveList = new List<BookModel>()
+        {
+            new BookModel() { Title = "Harry Potter and the Half-Blood Prince (Harry Potter  #6)",Author = "J.K. Rowling", Genre = "Fantasy/Adventure", PublishingYear = 2006},
+            new BookModel() { Title = "Harry Potter and the Half-Blood Prince (Harry Potter  #5)",Author = "J.K. Rowling", Genre = "Fantasy/Adventure", PublishingYear = 2005},
+            new BookModel() { Title = "The Ultimate Hitchhiker's Guide: Five Complete Novels and One Story (Hitchhiker's Guide to the Galaxy  #1-5)",Author = "Douglas Adams", Genre = "Science Fiction", PublishingYear = 2005},
+            new BookModel() { Title = "A Short History of Nearly Everything",Author = "Bill Bryson", Genre = "Non-fiction", PublishingYear = 2003},
+            new BookModel() { Title = "Bill Bryson's African Diary",Author = "Bill Bryson", Genre = "Non-fiction", PublishingYear = 2004},
+            new BookModel() { Title = "In a Sunburned Country",Author = "Bill Bryson", Genre = "Non-fiction", PublishingYear = 2005},
+            new BookModel() { Title = "Hatchet (Brian's Saga  #1)",Author = "Gary Paulsen", Genre = "Young adult novel", PublishingYear = 2015},
+            new BookModel() { Title = "Guts: The True Stories behind Hatchet and the Brian Books",Author = "Gary Paulsen", Genre = "Young adult novel", PublishingYear = 2010},
+            new BookModel() { Title = "Molly Hatchet - 5 of the Best",Author = "Molly Hatchet", Genre = "Non-fiction", PublishingYear = 2021},
+            new BookModel() { Title = "Hatchet Jobs: Writings on Contemporary Fiction",Author = "Dale Peck", Genre = "Non-fiction", PublishingYear = 2002},
+            new BookModel() { Title = "Uncommon Carriers",Author = "John McPhee", Genre = "Non-fiction", PublishingYear = 2004},
+            new BookModel() { Title = "Heirs of General Practice",Author = "John McPhee", Genre = "Non-fiction", PublishingYear = 2003},
+            new BookModel() { Title = "The Control of Nature",Author = "John McPhee", Genre = "Non-fiction", PublishingYear = 2005},
+            new BookModel() { Title = "The Heidi Chronicles",Author = "Wendy Wasserstein", Genre = "Drama", PublishingYear = 2011},
+            new BookModel() { Title = "The Heidi Chronicles: Uncommon Women and Others & Isn't It Romantic",Author = "Wendy Wasserstein", Genre = "Drama", PublishingYear = 2013},
+        };
+    }
+}
